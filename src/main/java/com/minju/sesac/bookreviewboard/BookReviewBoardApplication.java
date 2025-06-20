56eb47d3d70bea819a0af4fcb758a13517a5d6e2
xeimun <hinote444@naver.com>
2025-06-21 01:36:08 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/com/minju/sesac/bookreviewboard/BookReviewBoardApplication.java
@@ -0,0 +1,13 @@
+package com.minju.sesac.bookreviewboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookReviewBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minju/sesac/bookreviewboard/BookReviewBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minju.sesac.bookreviewboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
