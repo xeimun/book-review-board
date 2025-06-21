09adfe84091685ebc9038b6dfd6dd41d27c59f45
xeimun <hinote444@naver.com>
2025-06-21 15:13:36 +0900

    feat: 모든 리뷰 조회 API 구현

--- src/main/java/com/minju/sesac/bookreviewboard/controller/ReviewController.java
+++ src/main/java/com/minju/sesac/bookreviewboard/controller/ReviewController.java
@@ -1,11 +1,13 @@
 package com.minju.sesac.bookreviewboard.controller;
 
-import com.minju.sesac.bookreviewboard.dto.ReviewResponse;
 import com.minju.sesac.bookreviewboard.dto.ReviewCreateRequest;
+import com.minju.sesac.bookreviewboard.dto.ReviewResponse;
 import com.minju.sesac.bookreviewboard.service.ReviewService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/review")
+@RequestMapping("/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<ReviewResponse> create(@RequestBody ReviewCreateRequest request) {
         ReviewResponse response = reviewService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                              .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReviewResponse>> getAll() {
+        List<ReviewResponse> responses = reviewService.getAll();
+        return ResponseEntity.ok(responses);
     }
 }
--- src/main/java/com/minju/sesac/bookreviewboard/service/ReviewService.java
+++ src/main/java/com/minju/sesac/bookreviewboard/service/ReviewService.java
@@ -4,6 +4,8 @@ import com.minju.sesac.bookreviewboard.domain.Review;
 import com.minju.sesac.bookreviewboard.dto.ReviewCreateRequest;
 import com.minju.sesac.bookreviewboard.dto.ReviewResponse;
 import com.minju.sesac.bookreviewboard.repository.ReviewRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,5 +19,12 @@ public class ReviewService {
     public ReviewResponse create(ReviewCreateRequest request) {
         Review newReview = Review.from(request);
         return ReviewResponse.from(reviewRepository.save(newReview));
+    }
+
+    public List<ReviewResponse> getAll() {
+        List<Review> reviews = reviewRepository.findAll();
+        return reviews.stream()
+                      .map(review -> ReviewResponse.from(review))
+                      .collect(Collectors.toList());
     }
 }
